b4b95fb78d893ff75408a548baac18eacb109dcc
James White <whitejamesthe2nd@gmail.com>
2022-04-22 21:53:08 -0400

    local storage shennanigans

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React, {useState,useRef} from 'react';
+import React, {useState,useRef, useEffect} from 'react';
 import TodoList from './TodoList';
 import {v4 as uuidv4} from 'uuid';
+
+const localStorageKey = "todoApp.todos";
 
 function App() {
   const  [todos, setTodos] = useState([]);
   const todoNameRef = useRef();
+
+  useEffect(()=>{
+    const storedTodos = JSON.parse(localStorage.getItem(localStorageKey));
+    console.log(storedTodos);
+    if(storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect( ()=>{
+    localStorage.setItem(localStorageKey, JSON.stringify(todos));
+    let test = localStorage.getItem(localStorageKey);
+  }, [todos])
 
   let handleAddTodo = (e)=>{
     let name = todoNameRef.current.value;
